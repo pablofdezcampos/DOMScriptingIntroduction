423431c411c9237c2d94c4a54ec20056a4651c60
Pablo Fernández Campos <pablofdezcvc1@hotmail.com>
2022-03-17 10:19:38 +0100

    Generation of HTML code

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ const heading = document.querySelector('.header__text h2');
 heading.textContent = 'New Heading';
 
 
-//.querySelectorAll
+//.querySelectorAll -> document get reference for all de html code
 const links = document.querySelectorAll('.navegation a');
 links[0].textContent = 'Google link';
 links[0].href = 'https://google.com';
@@ -23,3 +23,28 @@ links[0].classList.remove('new-class');
 //.getElementById -> in the new versions do not have much use.
 const heading2 = document.getElementById('name');
 console.log(heading2);
+
+
+//Generation of a new link
+/**
+    Creation of HTML code
+    JS recomend to write the element in uppercase
+ */
+const newLink = document.createElement('A')
+
+//<a href="" class="">Text</a>
+
+//Adding href
+newLink.href = 'dom.html';
+
+//Adding text
+newLink.textContent = 'New link';
+
+//Adding class
+newLink.classList.add("navegation__link");
+
+//Adding element to the document
+const navegation = document.querySelector('.navegation');
+navegation.appendChild(newLink);
+
+console.log(newLink);
